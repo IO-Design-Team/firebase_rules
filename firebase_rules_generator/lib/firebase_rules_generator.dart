aba93ad79d793996157153c4489526eb33298e6e
Rexios <rexios@rexios.dev>
2023-01-17 21:03:01 -0500

    ...

--- firebase_rules_generator/lib/firebase_rules_generator.dart
+++ firebase_rules_generator/lib/firebase_rules_generator.dart
@@ -3,4 +3,8 @@ import 'package:firebase_rules_generator/src/rules_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
 /// Builds generators for `build_runner` to run
-Builder rules(BuilderOptions options) => LibraryBuilder(RulesGenerator());
+Builder rules(BuilderOptions options) => LibraryBuilder(
+      RulesGenerator(),
+      generatedExtension: '.rules',
+      formatOutput: (input) => input,
+    );
--- firebase_rules_generator/lib/src/rules_generator.dart
+++ firebase_rules_generator/lib/src/rules_generator.dart
@@ -1,17 +1,40 @@
+import 'dart:mirrors';
+
 import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/type.dart';
 import 'package:build/build.dart';
 import 'package:firebase_rules/firebase_rules.dart';
 import 'package:source_gen/source_gen.dart';
 
 /// Generate Firebase rules from a list of [Match] objects
 class RulesGenerator extends GeneratorForAnnotation<FirebaseRules> {
+  static final _matchClassName =
+      MirrorSystem.getName(reflectClass(Match().runtimeType).simpleName);
+
   @override
   String generateForAnnotatedElement(
     Element element,
     ConstantReader annotation,
     BuildStep buildStep,
   ) {
-    assert(element is List<Match>);
+    if (!isValidElement(element)) {
+      throw InvalidGenerationSourceError(
+        'The annotated element must be a List<Match>',
+        element: element,
+      );
+    }
     return 'This is a test';
+  }
+
+  /// Check that
+  bool isValidElement(Element element) {
+    final tlve = element as TopLevelVariableElement;
+    if (!tlve.type.isDartCoreList) {
+      return false;
+    }
+    // check that the list type is Match
+    final listType = tlve.type as ParameterizedType;
+    final classElement = listType.typeArguments.single.element as ClassElement;
+    return classElement.name == _matchClassName;
   }
 }
