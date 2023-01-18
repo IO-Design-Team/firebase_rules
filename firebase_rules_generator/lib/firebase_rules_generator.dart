3892c57cd7356cae644544047c1210e844652b87
Rexios <rexios@rexios.dev>
2023-01-17 19:20:31 -0500

    ...

--- firebase_rules_generator/lib/firebase_rules_generator.dart
+++ firebase_rules_generator/lib/firebase_rules_generator.dart
@@ -3,5 +3,4 @@ import 'package:firebase_rules_generator/src/rules_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
 /// Builds generators for `build_runner` to run
-Builder rules(BuilderOptions options) =>
-    LibraryBuilder(RulesGenerator(), generatedExtension: '.rules');
+Builder rules(BuilderOptions options) => LibraryBuilder(RulesGenerator());
--- firebase_rules_generator/lib/src/rules_generator.dart
+++ firebase_rules_generator/lib/src/rules_generator.dart
@@ -3,13 +3,15 @@ import 'package:build/build.dart';
 import 'package:firebase_rules/firebase_rules.dart';
 import 'package:source_gen/source_gen.dart';
 
+/// Generate Firebase rules from a list of [Match] objects
 class RulesGenerator extends GeneratorForAnnotation<FirebaseRules> {
   @override
-  generateForAnnotatedElement(
+  String generateForAnnotatedElement(
     Element element,
     ConstantReader annotation,
     BuildStep buildStep,
   ) {
-    
+    assert(element is List<Match>);
+    return 'This is a test';
   }
 }
