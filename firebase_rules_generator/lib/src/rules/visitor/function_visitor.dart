940ee8f4c8d519a60619fb258b678fd3f4fab5e6
Rexios <rexios@rexios.dev>
2023-01-18 16:46:18 -0500

    ...

--- firebase_rules_generator/lib/src/rules/visitor/function_visitor.dart
+++ firebase_rules_generator/lib/src/rules/visitor/function_visitor.dart
@@ -1,13 +1,48 @@
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:firebase_rules_generator/src/rules/rules_context.dart';
 import 'package:firebase_rules_generator/src/util.dart';
+import 'package:source_gen/source_gen.dart';
 
 /// Visit Function nodes
 Stream<String> visitFunction(
   RulesContext context,
   FunctionDeclaration node,
 ) async* {
-  final body = node.functionExpression.body;
+  final name = node.name.toString();
+  final parameters = node.functionExpression.parameters?.parameters
+      .map((e) => e.name)
+      .join(', ');
+  yield 'function $name($parameters) {'.indent(context.indent);
+  yield* _visitFunctionBody(context.dive(), node.functionExpression.body);
+  yield '}'.indent(context.indent);
+}
+
+Stream<String> _visitFunctionBody(
+  RulesContext context,
+  FunctionBody body,
+) async* {
   if (body is BlockFunctionBody) {
-  } else {}
+    final statements = body.block.statements;
+    for (final statement in statements) {
+      if (statement is VariableDeclarationStatement) {
+        final variable = statement.variables.variables.single;
+        final name = variable.name;
+        final initializer =
+            variable.initializer!.toSource().replaceAll('?', '');
+        yield 'let $name = $initializer;'.indent(context.indent);
+      } else if (statement is ReturnStatement) {
+        final expression = statement.expression;
+        yield 'return $expression;'.indent(context.indent);
+      } else {
+        throw InvalidGenerationSourceError(
+          'Unsupported function statement type: $statement',
+        );
+      }
+    }
+  } else if (body is ExpressionFunctionBody) {
+    final expression = body.expression;
+    yield '$expression;'.indent(context.indent);
+  } else {
+    throw InvalidGenerationSourceError('Unsupported function body type: $body');
+  }
 }
