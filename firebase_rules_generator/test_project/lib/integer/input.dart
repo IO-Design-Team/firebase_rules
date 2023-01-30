3b736743693ff06bf4e9a286ed86d5b2b7f384e3
Rexios <rexios@rexios.dev>
2023-01-29 23:38:54 -0500

    Integer test

---
+++ firebase_rules_generator/test_project/lib/integer/input.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: unused_local_variable
+
+import 'package:firebase_rules/firebase_rules.dart';
+import 'package:cloud_firestore_platform_interface/cloud_firestore_platform_interface.dart';
+
+@RulesFunction()
+bool test() {
+  final a = rules.parseInt('2'.rules);
+  final b = rules.parseInt(2.0);
+  return true;
+}
+
+@FirebaseRules(service: Service.firestore)
+final firestoreRules = <Match>[];
+
+abstract class BlobTest {
+  Blob get blob;
+}
