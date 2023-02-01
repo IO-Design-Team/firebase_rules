549eeb87a52948f7d9003ff8325bea144a226d76
Rexios <rexios@rexios.dev>
2023-02-01 14:20:07 -0500

    ...

--- firebase_rules_generator/test/database/util.dart
+++ firebase_rules_generator/test/database/util.dart
@@ -6,7 +6,7 @@ import 'package:firebase_rules_generator/firebase_rules_generator.dart';
 import 'package:test/test.dart';
 
 void testDatabaseRulesBuilder(String name) {
-  test(name, () async {
+  test('database/$name', () async {
     final input =
         File('test_project/lib/database/$name/input.dart').readAsStringSync();
     final output = File('test_project/lib/database/$name/output.rules.json')
--- firebase_rules_generator/test/firebase/util.dart
+++ firebase_rules_generator/test/firebase/util.dart
@@ -6,7 +6,7 @@ import 'package:firebase_rules_generator/firebase_rules_generator.dart';
 import 'package:test/test.dart';
 
 void testFirebaseRulesBuilder(String name) {
-  test(name, () async {
+  test('firebase/$name', () async {
     final input =
         File('test_project/lib/firebase/$name/input.dart').readAsStringSync();
     final output =
